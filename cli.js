4ba72715059261cf080fb5b622d7626a5ad2030f
Taisuke Fukuno <fukuno@jig.jp>
2024-05-22 10:12:22 +0900

    support q text

--- cli.js
+++ cli.js
@@ -1,10 +1,11 @@
 import { fetchImageRecog } from "./fetchImageRecog.js"
 
 const fn = Deno.args[0];
+const q = Deno.args[1];
 if (!fn) {
-  console.log("[jpeg fn]");
+  console.log("[jpeg fn] (q text)");
   Deno.exit(1);
 }
 const imgbin = new Uint8Array(await Deno.readFile(fn));
-const res = await fetchImageRecog(imgbin);
+const res = await fetchImageRecog(imgbin, q);
 console.log(res);
